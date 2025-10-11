781ab4ff8f722193b2779edc8ad317055d100a5c
Jens <jk224vx@student.lnu.se>
2025-10-11 10:49:25 +0200

    Added spring boot validation for setters.

--- src/main/java/com/jensprog/recipeconverter/model/ConversionRequest.java
+++ src/main/java/com/jensprog/recipeconverter/model/ConversionRequest.java
@@ -1,9 +1,21 @@
 package com.jensprog.recipeconverter.model;
 
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
 public class ConversionRequest {
+  @NotBlank(message = "Recipe name is required")
   private String recipeName;
+
+  @NotNull(message = "Amount is required")
+  @DecimalMin(value = "0.1", message = "Amount must be greater than 0")
   private double amount;
+
+  @NotBlank(message = "From unit is required")
   private String fromUnit;
+
+  @NotBlank(message = "To unit is required")
   private String toUnit;
 
   public ConversionRequest() {}
@@ -35,7 +47,7 @@ public class ConversionRequest {
     this.recipeName = recipeName;
   }
 
-  public void setAmount (double amount) {
+  public void setAmount(double amount) {
     this.amount = amount;
   }
 
